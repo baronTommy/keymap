80ca6a9aea5cb79cd0b077c73aaa8ccc556617cb
baronTommy <16768208+baronTommy@users.noreply.github.com>
2021-05-06 14:21:06 +0900

    fix fmt

--- handwired/dactyl/keymaps/my-keymap/keymap.c
+++ handwired/dactyl/keymaps/my-keymap/keymap.c
@@ -213,242 +213,258 @@ static bool one_shot_modifiers_through_flg = false;
 static bool is_input_mode_en = false;
 
 typedef struct OneShotModifier {
-  uint16_t key;
-  uint16_t send_key;
-  uint16_t down_ms;
-  int layer;
-  bool is_down;
+    uint16_t key;
+    uint16_t send_key;
+    uint16_t down_ms;
+    int layer;
+    bool is_down;
 } one_shot_modifier;
 one_shot_modifier one_shot_modifiers[] = {
-  {MY_KC_LL, KC_ENTER, MY_KC_LL, BASE, false},
-  {MY_KC_LR, KC_BSPACE, MY_KC_LR, BASE, false},
-  {MY_KC_RL, KC_ESCAPE, MY_KC_RL, BASE, false},
-  {MY_KC_RR, KC_SPACE, MY_KC_RR, BASE, false},
-  {MY_KC_FN, MY_KC_0, MY_KC_FN, FN, false},
-  {MY_KC_SYM1, MY_KC_0, MY_KC_SYM1, SYMBOL1, false},
-  {MY_KC_SYM2, MY_KC_0, MY_KC_SYM2, SYMBOL2, false},
-  {MY_KC_NUM, MY_KC_0, MY_KC_NUM, NUMBER, false},
-  {MY_KC_SPEC, MY_KC_0, MY_KC_SPEC, MY_FN, false},
+    { MY_KC_LL, KC_ENTER, MY_KC_LL, BASE, false },
+    { MY_KC_LR, KC_BSPACE, MY_KC_LR, BASE, false },
+    { MY_KC_RL, KC_ESCAPE, MY_KC_RL, BASE, false },
+    { MY_KC_RR, KC_SPACE, MY_KC_RR, BASE, false },
+    { MY_KC_FN, MY_KC_0, MY_KC_FN, FN, false },
+    { MY_KC_SYM1, MY_KC_0, MY_KC_SYM1, SYMBOL1, false },
+    { MY_KC_SYM2, MY_KC_0, MY_KC_SYM2, SYMBOL2, false },
+    { MY_KC_NUM, MY_KC_0, MY_KC_NUM, NUMBER, false },
+    { MY_KC_SPEC, MY_KC_0, MY_KC_SPEC, MY_FN, false },
 };
-int one_shot_modifiers_cnt = (sizeof(one_shot_modifiers)/sizeof(one_shot_modifier));
+int one_shot_modifiers_cnt = (sizeof(one_shot_modifiers) / sizeof(one_shot_modifier));
 
 typedef struct SimultaneouslyOneShotModifier {
-  uint16_t key1;
-  uint16_t key2;
-  uint16_t send_key;
-  uint16_t down_ms1;
-  uint16_t down_ms2;
+    uint16_t key1;
+    uint16_t key2;
+    uint16_t send_key;
+    uint16_t down_ms1;
+    uint16_t down_ms2;
 } simultaneously_one_shot_modifier;
 simultaneously_one_shot_modifier simultaneously_one_shot_modifiers[] = {
-  // LL LR
-  {MY_KC_LL, MY_KC_LR, MY_KC_JP, MY_KC_LL, MY_KC_LR},
-  {MY_KC_LR, MY_KC_LL, MY_KC_JP, MY_KC_LR, MY_KC_LL},
+    // LL LR
+    { MY_KC_LL, MY_KC_LR, MY_KC_JP, MY_KC_LL, MY_KC_LR },
+    { MY_KC_LR, MY_KC_LL, MY_KC_JP, MY_KC_LR, MY_KC_LL },
 
-  // RL RR
-  {MY_KC_RL, MY_KC_RR, MY_KC_EN, MY_KC_LL, MY_KC_LR},
-  {MY_KC_RR, MY_KC_RL, MY_KC_EN, MY_KC_LR, MY_KC_LL},
+    // RL RR
+    { MY_KC_RL, MY_KC_RR, MY_KC_EN, MY_KC_LL, MY_KC_LR },
+    { MY_KC_RR, MY_KC_RL, MY_KC_EN, MY_KC_LR, MY_KC_LL },
 
-  // LL RR
-  {MY_KC_LL, MY_KC_RR, MY_KC_19, MY_KC_LL, MY_KC_RR},
-  {MY_KC_RR, MY_KC_LL, MY_KC_19, MY_KC_RR, MY_KC_LL},
+    // LL RR
+    { MY_KC_LL, MY_KC_RR, MY_KC_19, MY_KC_LL, MY_KC_RR },
+    { MY_KC_RR, MY_KC_LL, MY_KC_19, MY_KC_RR, MY_KC_LL },
 
-  // LR RL
-  {MY_KC_LR, MY_KC_RL, MY_KC_20, MY_KC_LR, MY_KC_RL},
-  {MY_KC_RL, MY_KC_LR, MY_KC_20, MY_KC_RL, MY_KC_LR},
+    // LR RL
+    { MY_KC_LR, MY_KC_RL, MY_KC_20, MY_KC_LR, MY_KC_RL },
+    { MY_KC_RL, MY_KC_LR, MY_KC_20, MY_KC_RL, MY_KC_LR },
 
-  // LL RL
-  {MY_KC_LL, MY_KC_RL, MY_KC_21, MY_KC_LL, MY_KC_RL},
-  {MY_KC_RL, MY_KC_LL, MY_KC_21, MY_KC_RL, MY_KC_LL},
+    // LL RL
+    { MY_KC_LL, MY_KC_RL, MY_KC_21, MY_KC_LL, MY_KC_RL },
+    { MY_KC_RL, MY_KC_LL, MY_KC_21, MY_KC_RL, MY_KC_LL },
 
-  // LR RR
-  {MY_KC_LR, MY_KC_RR, MY_KC_22, MY_KC_LR, MY_KC_RR},
-  {MY_KC_RR, MY_KC_LR, MY_KC_22, MY_KC_RR, MY_KC_LR},
+    // LR RR
+    { MY_KC_LR, MY_KC_RR, MY_KC_22, MY_KC_LR, MY_KC_RR },
+    { MY_KC_RR, MY_KC_LR, MY_KC_22, MY_KC_RR, MY_KC_LR },
 };
-int simultaneously_one_shot_modifiers_cnt = (sizeof(simultaneously_one_shot_modifiers)/sizeof(simultaneously_one_shot_modifier));
+int simultaneously_one_shot_modifiers_cnt = (sizeof(simultaneously_one_shot_modifiers) / sizeof(simultaneously_one_shot_modifier));
 
-void set_input_mode(uint16_t keycode) {
-  if (keycode == MY_KC_EN) {
-    is_input_mode_en = true;
-  }
+void set_input_mode(uint16_t keycode)
+{
+    if (keycode == MY_KC_EN) {
+        is_input_mode_en = true;
+    }
 
-  if (keycode == MY_KC_JP) {
-    is_input_mode_en = false;
-  }
+    if (keycode == MY_KC_JP) {
+        is_input_mode_en = false;
+    }
 }
 
-void key_send(uint16_t keycode) {
-  set_input_mode(keycode);
-  one_shot_modifiers_through_flg = true;
-  register_code(keycode);
-  unregister_code(keycode);
+void key_send(uint16_t keycode)
+{
+    set_input_mode(keycode);
+    one_shot_modifiers_through_flg = true;
+    register_code(keycode);
+    unregister_code(keycode);
 }
 
-uint16_t get_diff_time_ms(uint16_t a, uint16_t b) {
+uint16_t get_diff_time_ms(uint16_t a, uint16_t b)
+{
     if (a > b) {
-      return a - b;
+        return a - b;
     }
     return b - a;
 }
 
-int find_one_shot_modifier_no(uint16_t keycode) {
-  for (int i = 0; i <= one_shot_modifiers_cnt; i++) {
-    if (one_shot_modifiers[i].key == keycode) {
-      return i;
+int find_one_shot_modifier_no(uint16_t keycode)
+{
+    for (int i = 0; i <= one_shot_modifiers_cnt; i++) {
+        if (one_shot_modifiers[i].key == keycode) {
+            return i;
+        }
     }
-  }
-  return -1;
+    return -1;
 }
 
-bool is_key_down(uint16_t keycode) {
-  int no = find_one_shot_modifier_no(keycode);
-  return one_shot_modifiers[no].is_down;
+bool is_key_down(uint16_t keycode)
+{
+    int no = find_one_shot_modifier_no(keycode);
+    return one_shot_modifiers[no].is_down;
 }
 
-bool can_one_shot_modifier(uint16_t keycode) {
-  int no = find_one_shot_modifier_no(keycode);
-  if (one_shot_modifiers_through_flg) {
-    return false;
-  }
-
-  if (timer_elapsed(one_shot_modifiers[no].down_ms) > ONE_SHOT_TIME) {
-    return false;
-  }
-
-  return true;
-}
-
-void layer_change(int layer, bool on) {
-  if (layer == BASE) {
-    return;
-  }
-  if (on) {
-    layer_on(layer);
-    return;
-  }
-  layer_off(layer);
-}
-
-void send_one_shot_modifier(uint16_t keycode) {
-  int no = find_one_shot_modifier_no(keycode);
-  key_send(one_shot_modifiers[no].send_key);
-}
-
-void set_modifier_key_down(uint16_t keycode) {
-  int no = find_one_shot_modifier_no(keycode);
-  layer_change(one_shot_modifiers[no].layer, true);
-
-  one_shot_modifiers[no].down_ms = timer_read();
-  one_shot_modifiers[no].is_down = true;
-  one_shot_modifiers_through_flg = false;
-}
-
-int find_simultaneously_one_shot_modifier_pear_no(uint16_t keycode) {
-  for (int i = 0; i <= simultaneously_one_shot_modifiers_cnt; i++) {
-    if (simultaneously_one_shot_modifiers[i].key1 != keycode) {
-      continue;
+bool can_one_shot_modifier(uint16_t keycode)
+{
+    int no = find_one_shot_modifier_no(keycode);
+    if (one_shot_modifiers_through_flg) {
+        return false;
     }
 
-    int pear_no = find_one_shot_modifier_no(simultaneously_one_shot_modifiers[i].key2);
-    if (pear_no > -1 && !one_shot_modifiers[pear_no].is_down) {
-      continue;
-    }
-    return pear_no;
-  }
-  return -1;
-}
-
-int find_simultaneously_one_shot_modifier_no(uint16_t key1, uint16_t key2) {
-  for (int i = 0; i <= simultaneously_one_shot_modifiers_cnt; i++) {
-    if (simultaneously_one_shot_modifiers[i].key1 == key1 && simultaneously_one_shot_modifiers[i].key2 == key2) {
-      return i;
+    if (timer_elapsed(one_shot_modifiers[no].down_ms) > ONE_SHOT_TIME) {
+        return false;
     }
 
-    if (simultaneously_one_shot_modifiers[i].key1 == key2 && simultaneously_one_shot_modifiers[i].key2 == key1) {
-      return i;
+    return true;
+}
+
+void layer_change(int layer, bool on)
+{
+    if (layer == BASE) {
+        return;
     }
-  }
-  return -1;
+    if (on) {
+        layer_on(layer);
+        return;
+    }
+    layer_off(layer);
 }
 
-bool can_simultaneously_one_shot_modifier(uint16_t keycode) {
-  if (one_shot_modifiers_through_flg) {
-    return false;
-  }
-
-  int pear_no = find_simultaneously_one_shot_modifier_pear_no(keycode);
-  if (pear_no == -1) {
-    return false;
-  }
-
-  int current_no = find_one_shot_modifier_no(keycode);
-  uint16_t current_down_time = timer_elapsed(one_shot_modifiers[current_no].down_ms);
-  if (current_down_time > ONE_SHOT_TIME) {
-    return false;
-  }
-
-  uint16_t pear_down_time = timer_elapsed(one_shot_modifiers[pear_no].down_ms);
-  return get_diff_time_ms(pear_down_time, current_down_time) < W_ONE_SHOT_DOWN_TIME;
+void send_one_shot_modifier(uint16_t keycode)
+{
+    int no = find_one_shot_modifier_no(keycode);
+    key_send(one_shot_modifiers[no].send_key);
 }
 
-void send_simultaneously_one_shot_modifier(uint16_t keycode) {
-  int current_no = find_one_shot_modifier_no(keycode);
-  int pear_no = find_simultaneously_one_shot_modifier_pear_no(keycode);
-  int no = find_simultaneously_one_shot_modifier_no(one_shot_modifiers[current_no].key, one_shot_modifiers[pear_no].key);
-  unregister_code(one_shot_modifiers[pear_no].key);
-  key_send(simultaneously_one_shot_modifiers[no].send_key);
+void set_modifier_key_down(uint16_t keycode)
+{
+    int no = find_one_shot_modifier_no(keycode);
+    layer_change(one_shot_modifiers[no].layer, true);
+
+    one_shot_modifiers[no].down_ms = timer_read();
+    one_shot_modifiers[no].is_down = true;
+    one_shot_modifiers_through_flg = false;
 }
 
-void set_modifier_key_up(uint16_t keycode) {
+int find_simultaneously_one_shot_modifier_pear_no(uint16_t keycode)
+{
+    for (int i = 0; i <= simultaneously_one_shot_modifiers_cnt; i++) {
+        if (simultaneously_one_shot_modifiers[i].key1 != keycode) {
+            continue;
+        }
+
+        int pear_no = find_one_shot_modifier_no(simultaneously_one_shot_modifiers[i].key2);
+        if (pear_no > -1 && !one_shot_modifiers[pear_no].is_down) {
+            continue;
+        }
+        return pear_no;
+    }
+    return -1;
+}
+
+int find_simultaneously_one_shot_modifier_no(uint16_t key1, uint16_t key2)
+{
+    for (int i = 0; i <= simultaneously_one_shot_modifiers_cnt; i++) {
+        if (simultaneously_one_shot_modifiers[i].key1 == key1 && simultaneously_one_shot_modifiers[i].key2 == key2) {
+            return i;
+        }
+
+        if (simultaneously_one_shot_modifiers[i].key1 == key2 && simultaneously_one_shot_modifiers[i].key2 == key1) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+bool can_simultaneously_one_shot_modifier(uint16_t keycode)
+{
+    if (one_shot_modifiers_through_flg) {
+        return false;
+    }
+
+    int pear_no = find_simultaneously_one_shot_modifier_pear_no(keycode);
+    if (pear_no == -1) {
+        return false;
+    }
+
+    int current_no = find_one_shot_modifier_no(keycode);
+    uint16_t current_down_time = timer_elapsed(one_shot_modifiers[current_no].down_ms);
+    if (current_down_time > ONE_SHOT_TIME) {
+        return false;
+    }
+
+    uint16_t pear_down_time = timer_elapsed(one_shot_modifiers[pear_no].down_ms);
+    return get_diff_time_ms(pear_down_time, current_down_time) < W_ONE_SHOT_DOWN_TIME;
+}
+
+void send_simultaneously_one_shot_modifier(uint16_t keycode)
+{
+    int current_no = find_one_shot_modifier_no(keycode);
+    int pear_no = find_simultaneously_one_shot_modifier_pear_no(keycode);
+    int no = find_simultaneously_one_shot_modifier_no(one_shot_modifiers[current_no].key, one_shot_modifiers[pear_no].key);
+    unregister_code(one_shot_modifiers[pear_no].key);
+    key_send(simultaneously_one_shot_modifiers[no].send_key);
+}
+
+void set_modifier_key_up(uint16_t keycode)
+{
     unregister_code(keycode);
     int no = find_one_shot_modifier_no(keycode);
     layer_change(one_shot_modifiers[no].layer, false);
 
     if (can_simultaneously_one_shot_modifier(keycode)) {
-      one_shot_modifiers[no].is_down = false;
-      send_simultaneously_one_shot_modifier(keycode);
-      return;
+        one_shot_modifiers[no].is_down = false;
+        send_simultaneously_one_shot_modifier(keycode);
+        return;
     }
 
     if (can_one_shot_modifier(keycode)) {
-      one_shot_modifiers[no].is_down = false;
-      send_one_shot_modifier(keycode);
-      return;
+        one_shot_modifiers[no].is_down = false;
+        send_one_shot_modifier(keycode);
+        return;
     }
 
     one_shot_modifiers[no].is_down = false;
 }
 
-void pre_send_hankaku(void) {
+void pre_send_hankaku(void)
+{
     for (int i = 0; i <= one_shot_modifiers_cnt; i++) {
-      if (one_shot_modifiers[i].is_down) {
-        unregister_code(one_shot_modifiers[i].key);
-      }
+        if (one_shot_modifiers[i].is_down) {
+            unregister_code(one_shot_modifiers[i].key);
+        }
     }
 
     key_send(MY_KC_EN);
 
     for (int i = 0; i <= one_shot_modifiers_cnt; i++) {
-      if (one_shot_modifiers[i].is_down) {
-        register_code(one_shot_modifiers[i].key);
-      }
+        if (one_shot_modifiers[i].is_down) {
+            register_code(one_shot_modifiers[i].key);
+        }
     }
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
+bool process_record_user(uint16_t keycode, keyrecord_t* record)
+{
+    switch (keycode) {
     // 親指モディファイ
     case MY_KC_LL:
     case MY_KC_LR:
     case MY_KC_RL:
     case MY_KC_RR:
-      if (record->event.pressed) {
-        register_code(keycode);
-        set_modifier_key_down(keycode);
+        if (record->event.pressed) {
+            register_code(keycode);
+            set_modifier_key_down(keycode);
+            return true;
+        }
+        unregister_code(keycode);
+        set_modifier_key_up(keycode);
         return true;
-      }
-      unregister_code(keycode);
-      set_modifier_key_up(keycode);
-      return true;
 
     // レイヤー切り変え
     case MY_KC_FN:
@@ -456,12 +472,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case MY_KC_SYM2:
     case MY_KC_NUM:
     case MY_KC_SPEC:
-      if (record->event.pressed) {
-        set_modifier_key_down(keycode);
+        if (record->event.pressed) {
+            set_modifier_key_down(keycode);
+            return true;
+        }
+        set_modifier_key_up(keycode);
         return true;
-      }
-      set_modifier_key_up(keycode);
-      return true;
 
     // 記号, 数値 ime off
     case KC_0:
@@ -506,27 +522,27 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case KC_RPRN:
     case KC_LCBR:
     case KC_RCBR:
-      if (!record->event.pressed) {
+        if (!record->event.pressed) {
+            return true;
+        }
+        one_shot_modifiers_through_flg = true;
+
+        if (is_input_mode_en) {
+            return true;
+        }
+
+        is_input_mode_en = true;
+
+        register_code(KC_LSHIFT);
+        register_code(KC_LCTRL);
+        register_code(KC_SCOLON);
+        unregister_code(KC_SCOLON);
+        unregister_code(KC_LCTRL);
+        unregister_code(KC_LSHIFT);
+
+        // pre_send_hankaku を使用するとバグがある為ハードコーディング
+        // pre_send_hankaku();
         return true;
-      }
-      one_shot_modifiers_through_flg = true;
-
-      if (is_input_mode_en) {
-        return true;
-      }
-
-      is_input_mode_en = true;
-
-      register_code(KC_LSHIFT);
-      register_code(KC_LCTRL);
-      register_code(KC_SCOLON);
-      unregister_code(KC_SCOLON);
-      unregister_code(KC_LCTRL);
-      unregister_code(KC_LSHIFT);
-
-      // pre_send_hankaku を使用するとバグがある為ハードコーディング
-      // pre_send_hankaku();
-      return true;
 
     // shift ダウン時に ime off
     case KC_A:
@@ -555,24 +571,24 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case KC_X:
     case KC_Y:
     case KC_Z:
-      if (!record->event.pressed) {
+        if (!record->event.pressed) {
+            return true;
+        }
+        one_shot_modifiers_through_flg = true;
+
+        if (is_input_mode_en) {
+            return true;
+        }
+
+        if (!is_key_down(MY_KC_LL)) {
+            return true;
+        }
+
+        is_input_mode_en = true;
+        pre_send_hankaku();
         return true;
-      }
-      one_shot_modifiers_through_flg = true;
+    }
 
-      if (is_input_mode_en) {
-        return true;
-      }
-
-      if (!is_key_down(MY_KC_LL)) {
-        return true;
-      }
-
-      is_input_mode_en = true;
-      pre_send_hankaku();
-      return true;
-  }
-
-  one_shot_modifiers_through_flg = true;
-  return true;
+    one_shot_modifiers_through_flg = true;
+    return true;
 }
